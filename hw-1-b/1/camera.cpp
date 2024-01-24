bb5128a65305dc3ba0efb09f5a72490b2504ae3f
DerpyMissile <lightwizard42@gmail.com>
2024-01-23 19:19:32 -0800

    WOOOOOOOOOO hw 2 done

--- hw-1-b/1/camera.cpp
+++ hw-1-b/1/camera.cpp
@@ -42,12 +42,35 @@ void Camera::Set_Resolution(const ivec2& number_pixels_input)
 vec3 Camera::World_Position(const ivec2& pixel_index)
 {
     vec3 result;
-    TODO;
-    int result1 = (pixel_index.x[0] * number_pixels[0]) + (number_pixels[0] / 2);
-    int result2 = (pixel_index.x[1] * number_pixels[1]) + (number_pixels[1] / 2);
-    int result3 = position[2];
-    result[0] = result1;
-    result[1] = result2;
-    result[2] = result3;
+    //TODO;
+    //TA office hour notes
+    /*
+    we have to start with main
+    from a general view, need to implement render_pixel
+        which is in render world
+        did that
+    gonna use that file a lot more    
+    render pixel is directly related to written assignment
+    takes a ray, takes index of pixel, and creates a ray from camera to that ray
+    passes that ray (gets the color of it) casts ray from camera to pixel and sets the color for that pixel in output.png
+    what we have is our input (index i and j)
+    we need to find a ray from the camera to that position
+        so we need to find position of camera
+        find position of pixel
+        define the ray
+    i have almost all of these
+    just need to find the position of pixel
+    oh yeah and debug pixel
+    -t files are debug info (like 00-t.txt)
+
+    ok its starting to make sense
+    so what cell center is is that it basically tells us where it is in 2d
+    i got this
+    film position is p
+    */
+    result = film_position;
+    vec2 tempVec = Cell_Center(pixel_index);
+    result += horizontal_vector * (tempVec[0]);
+    result += vertical_vector * (tempVec[1]);
     return result;
 }
--- hw-1-b/1/render_world.cpp
+++ hw-1-b/1/render_world.cpp
@@ -19,20 +19,31 @@ Render_World::~Render_World()
 std::pair<Shaded_Object,Hit> Render_World::Closest_Intersection(const Ray& ray) const
 {
     TODO;
-    
-    return {};
+    Shaded_Object obj;
+    Hit finalHit;
+    finalHit.dist = 99999;
+    for(Shaded_Object s : objects){
+        Hit tempHit = s.object->Intersection(ray, -1);
+        if(tempHit.dist < finalHit.dist){
+            finalHit.dist = tempHit.dist;
+            obj = s;
+        }
+        delete s.object;
+    }
+    return {obj, finalHit};
 }
 
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    TODO; // set up the initial view ray here
+    //TODO; // set up the initial view ray here
     vec3 direc = camera.World_Position(pixel_index) - camera.position;
     direc = direc.normalized();
     Ray ray(camera.position, direc);
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
-    Pixel_Print();
+    Debug_Scope scope; 
+    Pixel_Print("cast ray ",ray);
 }
 
 void Render_World::Render()
@@ -47,12 +58,16 @@ void Render_World::Render()
 vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth) const
 {
     vec3 color;
-    TODO; // determine the color here
-    std::pair<Shaded_Object,Hit> objectHit = Closest_Intersection(ray);
-    if(objectHit.second.dist < 0){
-        color = background_shader->Shade_Surface(this, ray, objectHit.second, objectHit.first.object->Intersection(ray, 0), objectHit.first.object->Normal(ray, objectHit.first.object->Intersection(ray, 0)));
-    }else{
-        color = objectHit.first.shader->Shade_Surface();
-    }
+    //TODO; // determine the color here
+    // std::pair<Shaded_Object,Hit> objectHit = Closest_Intersection(ray);
+    // if(objectHit.second.dist < 0){
+    //     color = background_shader->Shade_Surface(*this, ray, objectHit.second, ray.Point(objectHit.second.dist), objectHit.first.object->Normal(ray, objectHit.second), recursion_depth);
+    //     delete objectHit.first.object;
+    //     delete background_shader;
+    // }else{
+    //     color = objectHit.first.shader->Shade_Surface(*this, ray, objectHit.second, ray.Point(objectHit.second.dist), objectHit.first.object->Normal(ray, objectHit.second), recursion_depth);
+    //     delete objectHit.first.object;
+    //     delete objectHit.first.shader;
+    // }
     return color;
 }
